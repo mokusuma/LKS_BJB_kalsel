82f24a624cba629d2d599d248d367c8467415fc8
mokusuma <134350415+mokusuma@users.noreply.github.com>
2023-06-23 10:59:46 +0700

    update C2_transition

--- Module_speeedtest/C2_transition/main.js
+++ Module_speeedtest/C2_transition/main.js
@@ -1,1 +1,11 @@
-const 
+const hiddenContainer = document.querySelector('.hiddenContainer');
+const hidden = document.querySelector('.hidden');
+const img = document.querySelector('img');
+
+function containerSize(){
+    hiddenContainer.style.width = img.offsetWidth + 5 + 'px';
+    hiddenContainer.style.height = img.offsetHeight + 5 + 'px';
+}
+
+addEventListener('resize', containerSize);
+containerSize();
